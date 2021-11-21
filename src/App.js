3e138597934f2b0db4c08a3b8cd7954b97c69404
VictorMtzCodes <victormtzcodes@gmail.com>
2021-11-21 09:45:33 -0500

    installed swiperjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Movie & Shows v2!</h1>
+      <Router>
+        <Switch>
+          <h1>Movie & Shows v2!</h1>
+        </Switch>
+      </Router>
     </div>
   );
 }
