0fd4fff99f46b117ce2a816947b25dd6c540149c
VictorMtzCodes <victormtzcodes@gmail.com>
2021-11-21 09:25:41 -0500

    git init and created navbar files

---
+++ src/components/Stateful/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import popcorn from './popcorn.png'
+
+const Navbar = ({ backgroundColor, position }) => {
+    const [navActive, setNavActive] = useState(false);
+    const [navBackground, setNavBackground] = useState(false);
+
+    const addNavBackground = () => {
+        if (window.scrollY >= 80) {
+            setNavBackground(true)
+        } else {
+            setNavBackground(false)
+        }
+    }
+
+    window.addEventListener('scroll', addNavBackground);
+
+    return (
+        <div className={navBackground ? 'navbar active' : 'navbar'} style={{ position: position }}>
+            <span className='title'>
+                <h1 className='name'>StreamVic</h1>
+                <img className='navbar-img' src={popcorn} alt="popcorn" />
+            </span>
+            <div className="burger" onClick={() => setNavActive(!navActive)}>
+                <div className={navActive ? "line-1 toggle" : 'line-1'}></div>
+                <div className={navActive ? "line-2 toggle" : 'line-2'}></div>
+                <div className={navActive ? "line-3 toggle" : 'line-3'}></div>
+            </div>
+            <ul className={navActive ? 'nav-links active' : 'nav-links'}>
+                <Link to='/' style={{ color: '#fff', textDecoration: "none" }}>
+                    <li className='nav-link'>Home</li>
+                </Link>
+                <Link to='/movies' style={{ color: '#fff', textDecoration: "none" }}>
+                    <li className='nav-link'>Movies</li>
+                </Link>
+                <Link to='/tv' style={{ color: '#fff', textDecoration: "none" }}>
+                    <li className='nav-link'>TV Shows</li>
+                </Link>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
